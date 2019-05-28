1eae3724e0716754cbddb76d1cb11770b8993520
Iris <iris.wolfisberg@hotmail.fr>
2019-05-28 16:47:40 +0200

    Enlever fichier migration de nico

--- db/migrate/20190528124749_add_coordinates_to_destinations.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToDestinations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :destinations, :latitude, :float
-    add_column :destinations, :longitude, :float
-  end
-end
