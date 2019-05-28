77021695e754cad5f6bf042d510ee152a2275a37
nicolas varone <varonenico@gmail.com>
2019-05-28 15:30:20 +0200

    research with geolocalisation

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,11 @@
 class DestinationsController < ApplicationController
   def index
-    @destinations = Destination.new
+    if params["/destinations"].blank? || params["/destinations"]["location"].blank?
+      @destinations = Destination.all
+    else
+      location = params["/destinations"]["location"]
+      @destinations = Destination.near(location, 1000)
+    end
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_134127) do
+ActiveRecord::Schema.define(version: 2019_05_28_124749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_05_27_134127) do
     t.string "photo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
