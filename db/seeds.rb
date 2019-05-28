924631a8900423b34781ca77d1128369f8504b04
Justine Tincq <jtincq@gmail.com>
2019-05-28 14:44:08 +0200

    refracto seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
 
-puts "destroy all"
+puts "Destroy all"
 Destination.destroy_all
 User.destroy_all
 
-puts "create destinations"
+puts "Getting destinations from API"
 
-# cities = ["Bangkok", "London", "Paris", "Singapour", "New York", "Istanbul", "Dubaï", "Kuala Lumpur", "Rome", "Lisbon", "Prague", "Pekin", "Zurich", "Barcelona", "Madrid", "Moscow", "Budapest", "Berlin", "Vienna", "Munich"]
-# countries = ["Thailand", "England", "France", "Singapour", "USA", "Turkey", "Dubaï", "Malaysia", "Italy", "Portugal", "Czech Republic", "China", "Switzerland", "Spain", "Spain", "Russia", "Hongary", "Germany", "Austria", "Germany"]
-# lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ratione nam velit, a eveniet earum, fugit laudantium voluptates suscipit qui quisquam aspernatur officia repellat quaerat provident. Hic, nisi voluptates ducimus."
-
-# cities.each_with_index do |name, index|
-#   destination = Destination.new(name: name, description: lorem, country: countries[index])
-#   destination.photo_url = "https://res.cloudinary.com/dotvqg92r/image/upload/c_scale,h_500/v1558965633/slowtraveller/london_rrwf7y.jpg"
-#   destination.save
-# end
 url = 'france.json'
 destinations_json = open(url).read
-destinations = JSON.parse(destinations_json)
-destinations = destinations["results"]
+destinations = JSON.parse(destinations_json)["results"]
 
-puts "iterating"
+puts "Iterating and create destinations"
 
 destinations.each do |destination|
   Destination.create!(
