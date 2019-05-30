afae5152a068bb660be0e32346b1e8fef03fe03d
nicolas varone <varonenico@gmail.com>
2019-05-30 15:11:18 +0200

    WIP page favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    destination = params["dest"].to_i
+    destination = params["dest"]
     Favorite.create!(
       user: current_user,
-      destination_id: destination
+      destination_id: destination,
+      redirect_to: list_favorites_path
     )
+  end
+
+  def list
+    @favorites = Favorite.where(user: current_user)
   end
 end
