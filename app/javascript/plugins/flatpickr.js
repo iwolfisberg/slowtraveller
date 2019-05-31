c2fb5da252da368df6b6ce285e84a1c7030ab3e2
Iris <iris.wolfisberg@hotmail.fr>
2019-05-31 10:59:14 +0200

    Modifier les cartes des parcours

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,6 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
-flatpickr(".datepicker", {})
+flatpickr(".datepicker", {minDate: "today"})
 
 flatpickr(".timepicker", {enableTime: true, time_24hr: true, noCalendar: true})
