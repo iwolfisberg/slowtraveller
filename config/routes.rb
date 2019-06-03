7ad00d3a80c28b9a2878af58ec91fa7094f770cd
Iris <iris.wolfisberg@hotmail.fr>
2019-06-03 16:30:09 +0200

    Juste enlever 2 trucs qui servent é rien

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'steps/create'
-  get 'steps/traveldiary'
   devise_for :users
   root to: 'destinations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
