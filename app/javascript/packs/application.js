81eee42f2f2000d5e7153e1941525b2cc95194b0
Iris <iris.wolfisberg@hotmail.fr>
2019-06-05 16:09:25 +0200

    bla

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,20 +8,4 @@ import "../plugins/flatpickr"
 // import { initNavbarWhenScroll } from '../components/navbar'
 // initNavbarWhenScroll();
 
-$( document ).ready(function() {
 
-  // hide spinner
-  $(".spinner").hide();
-
-
-  // show spinner on AJAX start
-  $(document).ajaxStart(function(){
-    $(".spinner").show();
-  });
-
-  // hide spinner on AJAX stop
-  $(document).ajaxStop(function(){
-    $(".spinner").hide();
-  });
-
-});
