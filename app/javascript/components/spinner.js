25b9f2985e865d3805c8e6732b6717979331c8b6
Iris <iris.wolfisberg@hotmail.fr>
2019-06-06 09:22:31 +0200

    Ajouter image spinner

--- app/javascript/components/spinner.js
+++ app/javascript/components/spinner.js
@@ -1,1 +1,7 @@
-
+export const initSpinner = () => {
+  const submitSearch = document.querySelector(".submit-search");
+  submitSearch.addEventListener("click", (event) => {
+    const contentIndex = document.querySelector(".content-index");
+    contentIndex.insertAdjacentHTML("afterbegin", `<h3 class="text-spinner">Looking for destinations around you !</h3><img src="/assets/train.png">`)
+  });
+};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,9 @@ import "bootstrap";
 
 import "../plugins/flatpickr"
 
-// import { initCardDetailsOnClick } from '../components/card_click'
-// initCardDetailsOnClick();
-
 // import { initNavbarWhenScroll } from '../components/navbar'
 // initNavbarWhenScroll();
 
+import { initSpinner } from '../components/spinner'
+initSpinner();
 
