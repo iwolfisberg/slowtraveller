1b5e12d3a1b18454bb584b593462768cc4c86d2b
Iris <iris.wolfisberg@hotmail.fr>
2019-06-06 10:01:14 +0200

    Spinner fonctionne

--- app/javascript/components/spinner.js
+++ app/javascript/components/spinner.js
@@ -2,6 +2,7 @@ export const initSpinner = () => {
   const submitSearch = document.querySelector(".submit-search");
   submitSearch.addEventListener("click", (event) => {
     const contentIndex = document.querySelector(".content-index");
-    contentIndex.insertAdjacentHTML("afterbegin", `<h3 class="text-spinner">Looking for destinations around you !</h3><img src="/assets/train.png">`)
+    contentIndex.querySelector("#article_one_index").style.margin = '175px 0 0 0';
+    contentIndex.insertAdjacentHTML("afterbegin", `<div class="spinner"><h3 class="text-spinner">Looking for destinations around you !</h3><img class="spinner-train mb-3" src="/assets/train.png"></div>`)
   });
 };
