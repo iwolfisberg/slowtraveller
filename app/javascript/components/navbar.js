4ae22ab32a958d9b4d22af308bfed98bccba82a5
Iris <iris.wolfisberg@hotmail.fr>
2019-06-06 16:26:24 +0200

    Modifier navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,12 @@
-// export const initNavbarWhenScroll = () => {
-//   const navbar = document.querySelector('.navbar-lewagon');
-//   if (navbar) {
-//     window.addEventListener('scroll', () => {
-//       if (window.scrollY >= window.innerHeight) {
-//         navbar.classList.add('navbar-lewagon-white');
-//       } else {
-//         navbar.classList.remove('navbar-lewagon-white');
-//       }
-//     });
-//   }
-// };
+export const initNavbarWhenScroll = () => {
+  const navbar = document.querySelector('.navbar-lewagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-lewagon-white');
+      } else {
+        navbar.classList.remove('navbar-lewagon-white');
+      }
+    });
+  }
+};
