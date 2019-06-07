82fd9ee791b4977438b87be84d8260dcfef4e6f2
Justine Tincq <jtincq@gmail.com>
2019-06-07 09:25:20 +0200

    delete raise

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -3,9 +3,8 @@ require 'yaml'
 class StepsController < ApplicationController
   def create
     info_step = params["info_step"]
-    km_total = 0
-    info_step[:journey][:steps].each { |step| km_total += step[:km].to_i }
-    raise
+    km = 0
+    info_step[:journey][:steps].each { |step| km += step[:km].to_i }
     @step = Step.create!(
       user: current_user,
       destination_id: info_step[:id],
@@ -14,7 +13,7 @@ class StepsController < ApplicationController
       departure: info_step[:journey][:departure],
       arrival: info_step[:journey][:arrival],
       arrival_day: info_step[:journey][:arrival_day],
-      km_total: km_total
+      km: km
     )
     redirect_to traveldiary_steps_path
   end
@@ -23,6 +22,7 @@ class StepsController < ApplicationController
     @steps = Step.where(user: current_user)
     @carbon_total = 0
     @steps.each { |step| @carbon_total += step[:carbon] }
+
     @first_departure = @steps.first["departure"].split(",")[0] if @steps.size.positive?
     @location_user = Destination.find(@steps.last.destination_id).name if @steps.size.positive?
   end
